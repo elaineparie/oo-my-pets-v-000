8856269f8e70797ee81aa7df2c4fbbf1918ddb2d
Elaine Anderson <eparie@bu.edu>
2018-06-05 18:55:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,14 +60,12 @@ pets[:fishes][0].mood = "happy"
 end
 
 def sell_pets
-  @pets.clear
     @pets[:fishes][0].mood = "nervous"
     @pets[:dogs][0].mood = "nervous"
     @pets[:cats][0].mood = "nervous"
 
-    binding.pry
 
-  
+
 
 end
 
