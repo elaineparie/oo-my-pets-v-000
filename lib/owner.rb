3e8889b219363705e081e7cd3c35f7d67c41198d
Elaine Anderson <eparie@bu.edu>
2018-06-05 17:40:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,19 @@
 class Owner
+attr_accessor :owner
 
 @@all = []
 
 def initialize
-  @@all << self
-end 
+  @@all << self.owner
+end
+
+def self.all
+  @@all
+end
+
+def reset_owner
+  @@all.clear
+end
+
 
 end
